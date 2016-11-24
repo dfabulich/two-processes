ea6ff57e12708b32a54cc36be04ce7393287032a
Dan Fabulich <dan@fabulich.com>
2016-11-23 22:01:23 -0800

    initial commit

---
+++ x/X.java
@@ -0,0 +1,5 @@
+public class X {
+  public static void main(String[] args) throws Exception {
+    Thread.sleep(30000L);
+  }
+}
---
+++ y/Y.java
@@ -0,0 +1,5 @@
+public class Y {
+  public static void main(String[] args) throws Exception {
+    Thread.sleep(30000L);
+  }
+}
